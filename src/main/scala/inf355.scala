9f85dce3a70691f1e81652e3027a12444227b189
Alexandre Bertails <bertails@gmail.com>
2014-06-10 16:02:43 +0200

    visitor pattern with generics

--- src/main/scala/inf355.scala
+++ src/main/scala/inf355.scala
@@ -1,52 +1,36 @@
 package net.rfc1149.inf355
 
 trait Expr {
-  def accept(visitor: Visitor): Unit
+  def accept[A](visitor: Visitor[A]): A
 }
 
-trait Visitor {
-  def visit(num: Num): Unit
-  def visit(mul: Mul): Unit
-  def visit(sum: Sum): Unit
+trait Visitor[A] {
+  def visit(num: Num): A
+  def visit(mul: Mul): A
+  def visit(sum: Sum): A
 }
 
 class Num(val i: Int) extends Expr {
-  def accept(visitor: Visitor) = visitor.visit(this)
+  def accept[A](visitor: Visitor[A]) = visitor.visit(this)
 }
 
 class Mul(val l: Expr, val r: Expr) extends Expr {
-  def accept(visitor: Visitor) = visitor.visit(this)
+  def accept[A](visitor: Visitor[A]) = visitor.visit(this)
 }
 
 class Sum(val l: Expr, val r: Expr) extends Expr {
-  def accept(visitor: Visitor) = visitor.visit(this)
+  def accept[A](visitor: Visitor[A]) = visitor.visit(this)
 }
 
 object Main extends App {
 
-  class Evaluator(var acc: Int = 0) extends Visitor {
-    def visit(num: Num): Unit = {
-      acc = num.i
-    }
-    def visit(mul: Mul): Unit = {
-      mul.l.accept(this)
-      val leftValue = acc
-      mul.r.accept(this)
-      acc *= leftValue
-    }
-    def visit(sum: Sum): Unit = {
-      sum.l.accept(this)
-      val leftValue = acc
-      sum.r.accept(this)
-      acc += leftValue
-    }
+  class Evaluator extends Visitor[Int] {
+    def visit(num: Num): Int = num.i
+    def visit(mul: Mul): Int = mul.l.accept(this) * mul.r.accept(this)
+    def visit(sum: Sum): Int = sum.l.accept(this) + sum.r.accept(this)
   }
 
-  def eval(e: Expr): Int = {
-    val evaluator = new Evaluator
-    e.accept(evaluator)
-    evaluator.acc
-  }
+  def eval(e: Expr): Int = e.accept(new Evaluator)
 
   val expr: Expr = new Sum(new Mul(new Num(1), new Num(2)), new Num(3))
 
