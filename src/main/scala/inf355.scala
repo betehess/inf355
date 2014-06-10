95f9f95d6dcb0fe3b003d460d4d9638bee9fd77d
Alexandre Bertails <bertails@gmail.com>
2014-06-10 15:37:35 +0200

    subtyping

--- src/main/scala/inf355.scala
+++ src/main/scala/inf355.scala
@@ -1,23 +1,25 @@
 package net.rfc1149.inf355
 
-trait Expr
+trait Expr {
+  def eval(): Int
+}
 
-class Num(val i: Int) extends Expr
+class Num(val i: Int) extends Expr {
+  def eval(): Int = i
+}
 
-class Mul(val l: Expr, val r: Expr) extends Expr
+class Mul(val l: Expr, val r: Expr) extends Expr {
+  def eval(): Int = l.eval() * r.eval()
+}
 
-class Sum(val l: Expr, val r: Expr) extends Expr
+class Sum(val l: Expr, val r: Expr) extends Expr {
+  def eval(): Int = l.eval() + r.eval()
+}
 
 object Main extends App {
 
   val expr: Expr = new Sum(new Mul(new Num(1), new Num(2)), new Num(3))
 
-  def eval(e: Expr): Int = e match {
-    case num: Num => num.i
-    case mul: Mul => eval(mul.l) * eval(mul.r)
-    case sum: Sum => eval(sum.l) + eval(sum.r)
-  }
-
-  println(eval(expr))
+  println(expr.eval())
 
 }
