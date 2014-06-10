a0e57050d1ee42625c33e97d39d46a14b8099589
Alexandre Bertails <bertails@gmail.com>
2014-06-10 15:32:31 +0200

    classes as tags

--- src/main/scala/inf355.scala
+++ src/main/scala/inf355.scala
@@ -1,2 +1,21 @@
 package net.rfc1149.inf355
 
+class Num(val i: Int)
+
+class Mul(val l: Any, val r: Any)
+
+class Sum(val l: Any, val r: Any)
+
+object Main extends App {
+
+  val expr = new Sum(new Mul(new Num(1), new Num(2)), new Num(3))
+
+  def eval(e: Any): Int = e match {
+    case num: Num => num.i
+    case mul: Mul => eval(mul.l) * eval(mul.r)
+    case sum: Sum => eval(sum.l) + eval(sum.r)
+  }
+
+  println(eval(expr))
+
+}
